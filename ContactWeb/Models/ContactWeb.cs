e3dac6b1065641885628afa282745734e2fa671b
gavvarux <gautam.avvaru@vsnc.co.uk>
2018-02-16 01:31:20 +0000

    km

---
+++ ContactWeb/Models/ContactWeb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactWeb.Models
+{
+    public class ContactWeb
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string phone2 { get; set; }
+        public DateTime dob { get; set; }
+        public string streetadd { get; set; }
+        public string streetadd2 { get; set; }
+        public string city { get; set; }
+        public string county { get; set; }
+        public string postcode { get; set; }
+    }
+}
